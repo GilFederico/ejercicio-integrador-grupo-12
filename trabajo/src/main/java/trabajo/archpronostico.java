c56b89c408173c8f60999d5d7d7e1fedc954ecb8
Gil Federico <fedegil2013@gmail.com>
2023-03-21 21:44:40 -0300

    cambios

--- trabajo/src/main/java/trabajo/archpronostico.java
+++ trabajo/src/main/java/trabajo/archpronostico.java
@@ -6,11 +6,7 @@ public class archpronostico {
 	@CsvBindByPosition ( position = 0)
 	private String equipo1;
 	@CsvBindByPosition ( position = 1)
-	private String gana1;
-//	@CsvBindByPosition ( position = 2)
-	//private char empate;
-	//@CsvBindByPosition ( position = 3)
-	//private char gana2;
+	private String gana;
 	@CsvBindByPosition (position =2)
 	private String equipo2;
 	
@@ -18,28 +14,22 @@ public class archpronostico {
 		this.equipo1=e1;
 	}
 	public void gana1(String g1) {
-		this.gana1=g1;
+		this.gana=g1;
 	}
-/*	public void empate(char e) {
-		this.empate=e;
-	}
-	public void equi2(char g2) {
-		this.gana2=g2;
-	}*/
+
 	public void equipo2(String e2) {
 		this.equipo2=e2;
 	}
 	
+	public String getgana() {
+		return gana;
+	}
 	public String gete1() {
-		return gana1;
+		return equipo1;
 	}
-	/*return gana1;
-	}
-	public char geatempata
-	public int getgana2() {
-		return gana2;
-	}
+
 	public String gete2() {
 		return equipo2;
-	}*/
+	}
+
 }
